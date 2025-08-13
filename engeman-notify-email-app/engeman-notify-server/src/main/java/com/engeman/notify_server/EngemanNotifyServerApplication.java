b629895122e6dc489d662b26219f6d596dfde15f
gui <paulogui702@gmail.com>
2025-08-13 13:42:39 -0300

    pilot

---
+++ engeman-notify-email-app/engeman-notify-server/src/main/java/com/engeman/notify_server/EngemanNotifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.engeman.notify_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngemanNotifyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngemanNotifyServerApplication.class, args);
+	}
+
+}
---
+++ engeman-notify-email-app/engeman-notify-server/src/test/java/com/engeman/notify_server/EngemanNotifyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeman.notify_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngemanNotifyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
